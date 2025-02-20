7cbbb5dd0dff14d4cdf46c1f7d2fd37058c59155
Beck kapellas <144378781+Blk027@users.noreply.github.com>
2025-02-19 22:51:34 -0800

    Update modifications.js

--- mods/modifications.js
+++ mods/modifications.js
@@ -332,3 +332,24 @@ function onLoadMods() {
 function makeLoadModsButton() {
     return div('listing', format('<a class="option smallFancyButton" ${}="${};PlaySound(\'snd/tick.mp3\');">${}</a><label>(${})</label>', Game.clickStr, 'onLoadMods()', 'Load mod link(s)', 'directly input mod links to load them into Cookie Clicker'));
 };
+
+Game.registerMod("spawngolden", {
+  init: function () {
+    AddEvent(window, 'keydown', function (e) {
+      // ctrl-g spanws a golden cookie
+      if (!Game.OnAscend && Game.AscendTimer == 0 && e.ctrlKey && e.keyCode == 71) {
+        for (var i in Game.shimmerTypes) {
+          var me = Game.shimmerTypes[i];
+          // no shimmer spawned for this type? check the timer and try to spawn one
+          if (me.spawnConditions() && !me.spawned) {
+            var newShimmer = new Game.shimmer(i);
+            newShimmer.spawnLead = 1;
+            if (Game.Has('Distilled essence of redoubled luck') && Math.random() < 0.01) var newShimmer = new Game.shimmer(i);
+            me.spawned = 1;
+          }
+        }
+        e.preventDefault();
+      }
+    });   
+  }
+});
